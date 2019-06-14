04b8d765ff33d964427f842b8040727b38129a4f
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-14 13:37:13 +0100

    http response promise js updated

--- scripts/httpRequest.js
+++ scripts/httpRequest.js
@@ -2,6 +2,15 @@ const makeRequest = (search) => {
     return new Promise((res, rej) => {
         let request = new XMLHttpRequest();
 
+        request.onload = () {
+            if (request.status >= 200 && request.status <= 299) {
+                resolve(request.response);
+            } else {
+                const reason = new Error("Oops, something went wrong! Error code: " + request.status);
+                reject(reason);
+            }
+        };
         request.open("GET", "http://www.omdbapi.com/?apikey=13510c0e".concat(search));
-    })
-}
+        request.send();
+    });
+};

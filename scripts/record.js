27d4e60e8a1ef1555ce19dc4779e85993427f197
Monika-Mistry <monika.mistry1@gmail.com>
2019-06-14 15:14:32 +0100

    js functionality to get sessionstorage variable

---
+++ scripts/record.js
@@ -0,0 +1,6 @@
+const getValues = () => {
+    document.getElementById('title').innerHTML = sessionStorage.getItem('title');
+    document.getElementById('year').innerHTML = sessionStorage.getItem('year');
+    // document.getElementById('title').innerHTML = sessionStorage.getItem('title');
+    document.getElementById('poster').src = sessionStorage.getItem('poster');
+}
